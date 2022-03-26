749e47e38a2c7040cf850e0c2c8ce1d3a79a615a
cerissad <cerissad@gmail.com>
2022-03-26 17:42:14 -0500

    fahreinheit to celsius conversion

--- src/index.js
+++ src/index.js
@@ -40,11 +40,20 @@ function showTemperature(response) {
   let mainCity = document.querySelector("#city-header");
   let humidityDetail = document.querySelector("#humidityDetail");
   let windSpeed = document.querySelector("#windSpeed");
+
+  fahreinheitTemperature = response.data.main.temp;
+
   mainTemp.innerHTML = Math.round(response.data.main.temp);
   mainCity.innerHTML = response.data.name;
   mainDescription.innerHTML = response.data.weather[0].main;
   humidityDetail.innerHTML = response.data.main.humidity;
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  weatherIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+  celsius.setAttribute("class", "active");
+  fahreinheit.setAttribute("class", null);
 }
 
 function getLoc() {
@@ -62,23 +71,52 @@ function enterCity(event) {
       return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-    cityHeader.innerHTML = capitalizeFirstLetter(`${cityInput.value}`);
+    cityHeader.innerHTML = `${cityInput.value}`;
+    celsius.setAttribute("class", "active");
+    fahreinheit.setAttribute("class", null);
   }
 
   function getTemperature(response) {
     let searchTemp = document.querySelector("#mainTemp");
     let cityDescr = document.querySelector("#weatherDescription");
+
+    fahreinheitTemperature = response.data.main.temp;
+
     humidityDetail = document.querySelector("#humidityDetail");
     windSpeed = document.querySelector("#windSpeed");
     searchTemp.innerHTML = Math.round(response.data.main.temp);
     cityDescr.innerHTML = response.data.weather[0].main;
     humidityDetail.innerHTML = response.data.main.humidity;
     windSpeed.innerHTML = Math.round(response.data.wind.speed);
+    weatherIcon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+    );
   }
 
   let searchURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=imperial&appid=${apiKey}`;
   axios.get(searchURL).then(getTemperature);
 }
+
+function convertCel(event) {
+  event.preventDefault();
+  let celsiusTemp = (fahreinheitTemperature - 32) * 0.5556;
+  let mainTemp = document.querySelector("#mainTemp");
+  mainTemp.innerHTML = Math.round(celsiusTemp);
+  fahreinheit.setAttribute("class", "active");
+  celsius.setAttribute("class", null);
+}
+
+function convertFah(event) {
+  event.preventDefault();
+  let fahTemp = fahreinheitTemperature;
+  let mainTemp = document.querySelector("#mainTemp");
+  mainTemp.innerHTML = Math.round(fahTemp);
+  celsius.setAttribute("class", "active");
+  fahreinheit.setAttribute("class", null);
+}
+
+navigator.geolocation.getCurrentPosition(handlePosition);
 
 let button = document.querySelector("button");
 button.addEventListener("click", getLoc);
@@ -86,4 +124,8 @@ button.addEventListener("click", getLoc);
 let form = document.querySelector("#city-input");
 form.addEventListener("submit", enterCity);
 
-navigator.geolocation.getCurrentPosition(handlePosition);
+let fahreinheitLink = document.querySelector("#fahreinheit");
+fahreinheitLink.addEventListener("click", convertFah);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", convertCel);
